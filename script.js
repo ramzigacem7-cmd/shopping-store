6dc80ab0d4e3493a79272085fe103d42cbacf684
ramzigacem7-cmd <ramzigacem7@gmail.com>
2026-04-26 17:25:27 +0100

    up

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// verify if the access or(login) information are exist
+
 let username = "ramzi";
 let password = "1234";
 const user_name = document.getElementById("username");
@@ -31,9 +33,10 @@ if (total_btn) {
   });
 }
 
+// open and close the shopping card
+
 const shop_btn = document.getElementById("shopping_card_btn");
 const shop_card = document.querySelector(".shopping_card");
-
 const exit_btn = document.getElementById("exit_btn");
 
 shop_btn.addEventListener("click", () => {
@@ -43,3 +46,58 @@ shop_btn.addEventListener("click", () => {
 exit_btn.addEventListener("click", () => {
   shop_card.style.display = "none";
 });
+
+const countity_in_card = document.getElementById("countity_in_card");
+
+const first_added_pro = document.getElementById("added_product");
+const remove_item = document.querySelector(".remove_item");
+
+const products_cards = document.querySelectorAll(".product_card");
+
+const shop_item = document.querySelector(".shop_items");
+
+function create_shop_cart() {
+  shop_item.innerHTML = "";
+  let card = JSON.parse(localStorage.getItem("card")) || [];
+  card.forEach((x) => {
+    let new_div = document.createElement("div");
+    new_div.classList.add("card_item");
+    new_div.innerHTML = `
+          <div>
+            <button type="button" class="add_btn">+</button>
+            <h3 style="color: white;"> ${x.countity}</h3>
+             <button type="button" class="remove_btn">-</button>
+            <h4 style="color: white;">total price:<ins style="color: lightgreen">${x.price}</ins>$</h4>
+          </div>
+          <div>
+            <h2 style="color: white;"> ${x.name}</h2>
+          </div>
+        <br>
+       `;
+    shop_item.appendChild(new_div);
+  });
+}
+
+products_cards.forEach((product) => {
+  let btn = product.querySelector(".add_to_card_btn");
+  btn.addEventListener("click", () => {
+    name = product.querySelector(".name").textContent;
+    price = parseInt(product.querySelector(".price").textContent);
+    countity = parseInt(product.querySelector(".countity").value);
+    let card = JSON.parse(localStorage.getItem("card")) || [];
+    let exist = card.find((item) => item.name === name);
+    if (exist) {
+      exist.countity += countity;
+      exist.price = price * exist.countity;
+    } else {
+      card.push({
+        name,
+        price,
+        countity,
+      });
+    }
+    localStorage.setItem("card", JSON.stringify(card));
+    create_shop_cart();
+  });
+});
+create_shop_cart();
